3855e4043b25d7e5e5c9c6357f5eb825367f9e3a
kmsmahi <salauddinmahi2002@gmail.com>
2026-08-07 11:36:13 +0600

    header-2

---
+++ src/Componenets/Marque.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Marque = () => {
+    return (
+        <div className='flex gap-5 items-center bg-[#F3F3F3] px-3 py-2'>
+            <p className='text-white bg-[#D72050] p-4'>Latest</p>
+            <p className='font-bold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, nesciunt.</p>
+        </div>
+    );
+};
+
+export default Marque;
--- src/LayOuts/HomeLayOuts.jsx
+++ src/LayOuts/HomeLayOuts.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Componenets/Header';
+import Marque from '../Componenets/Marque';
 
 const HomeLayOuts = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto mt-5'>
+                    <Marque></Marque>
+                </section>
             </header>
             <main>
                 <section className='left-nav'></section>
